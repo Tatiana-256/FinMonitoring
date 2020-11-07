4bd162b757a6de93ff08639d80f50a90841189f1
sem256 <motia256@gmail.com>
2020-11-07 11:44:37 +0100

    Added .net backend part

---
+++ back.net/FinMonitoring/WebApi/Models/Fund.cs
@@ -0,0 +1,23 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi.Models
+{
+    public class Fund
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        [BsonElement("Name")]
+        public string FundName { get; set; }
+
+        public decimal Amount { get; set; }
+
+        public decimal Goal { get; set; }
+    }
+}
