bf9537baeff82a3e143d92fa82bbe039f06f6166
Tatiana-256 <tatiana.beznosiuk@gmail.com>
2020-11-12 11:39:23 +0200

    add adding sumForm

--- front/fin-monitoring-front/src/components/Fund/Fund.tsx
+++ front/fin-monitoring-front/src/components/Fund/Fund.tsx
@@ -1,7 +1,8 @@
-import React from 'react'
+import React, {useState} from 'react'
 import styles from './Fund.module.css'
 import {IFund} from '../../redux/fund-reduser';
 import {Button} from "../../common-components/customButton/button";
+import {AddingSum} from "../addingSum/addingSum";
 
 
 interface IPropsStore {
@@ -10,20 +11,35 @@ interface IPropsStore {
 }
 
 export const Fund: React.FC<IPropsStore> = ({fund, deleteFund}) => {
-    return <div className={styles.fund}>
-        <div className={styles.options}>{fund.fundName}</div>
-        <div className={styles.options}>{fund.amount}</div>
-        <div className={styles.options}>{fund.currency}</div>
-        <div className={styles.options}>{fund.goal}</div>
-        <div className={styles.options}>{Math.round((fund.amount / fund.goal) * 100)}%</div>
-        <div
-            className={styles.delete_button}
-        >
-            <Button buttonClass={'red'} onClick={() => deleteFund!(fund.id)}
-            >Delete
-            </Button>
+
+    const [addingSum, setAddingSum] = useState(false)
+
+    const setForm = () => setAddingSum(prevState => !prevState)
+
+    return <>
+        {addingSum && <AddingSum setForm={setForm}/>}
+        <div className={styles.fund}>
+            <div className={styles.options}>{fund.fundName}</div>
+            <div className={`${styles.options} ${styles.amount}`}>
+                <div>
+                    {fund.amount}
+                </div>
+                <div className={styles.add_amount} onClick={setForm}>+</div>
+            </div>
+            <div className={styles.options}>{fund.currency}</div>
+            <div className={styles.options}>{fund.goal}</div>
+            <div
+                className={styles.options}>{fund.goal === 0 ? "Put goal amount" : `${Math.round((fund.amount / fund.goal) * 100)}%`}
+            </div>
+            <div
+                className={styles.delete_button}
+            >
+                <Button buttonClass={'red'} onClick={() => deleteFund!(fund.id)}
+                >Delete
+                </Button>
+            </div>
         </div>
-    </div>
+    </>
 }
 
 // `${styles.description} ${styles.yellow}`
