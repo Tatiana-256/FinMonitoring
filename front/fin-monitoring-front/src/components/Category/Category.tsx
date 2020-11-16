f77df3e2f4bdbcfb72c5ec32e9e7d8c599d211e5
Tatiana-256 <tatiana.beznosiuk@gmail.com>
2020-11-16 20:08:16 +0200

    add Category.tsx components

--- front/fin-monitoring-front/src/components/Category/Category.tsx
+++ front/fin-monitoring-front/src/components/Category/Category.tsx
@@ -1,7 +1,30 @@
-import React, {ChangeEvent, FC, useState} from 'react'
+import React, {useEffect} from 'react'
 import {AddItem} from "../../common-components/addItem/Add-item";
+import {getCategory} from "../../Fin-Redux/Category/Category-actions";
+import {useDispatch, useSelector} from "react-redux";
+import {AppStateType} from '../../Fin-Redux/store';
+import {Table} from "../../common-components/CommonTable/CommonTable";
+import styles from "./Category.module.css";
 
 
 export const Category = () => {
-    return <AddItem name={"Category"}/>
+    useEffect(() => {
+            console.log("hey")
+            dispatch(getCategory())
+        }, []
+    )
+
+    const dispatch = useDispatch();
+    const {category} = useSelector((store: AppStateType) => store)
+
+
+    console.log(category)
+
+    return <>
+        <AddItem name={"Category"}/>
+        <div className={styles.category_container}>
+            <Table name={"Category name"} id={"ID"} tableHeader={true}/>
+            {category.category.map(x => <Table name={x.categoryName} id={x.id} key={x.id} tableHeader={false}/>)}
+        </div>
+    </>
 }
