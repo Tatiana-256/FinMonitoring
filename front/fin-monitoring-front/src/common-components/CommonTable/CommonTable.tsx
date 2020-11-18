25337338588b6af042c643d458e439a34e30f6b8
Tatiana-256 <tatiana.beznosiuk@gmail.com>
2020-11-18 10:32:58 +0200

    edditing existing category

--- front/fin-monitoring-front/src/common-components/CommonTable/CommonTable.tsx
+++ front/fin-monitoring-front/src/common-components/CommonTable/CommonTable.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {ChangeEvent, useState} from "react";
 import styles from "./CommonTable.module.css";
 import {Button} from "../customButton/button";
 import {useDispatch} from "react-redux";
@@ -16,26 +16,52 @@ export const Table: React.FC<IProps> = ({name, id, tableHeader, editCategory}) =
     const dispatch = useDispatch();
 
     const [edit, setEdit] = useState(false)
+    const [changeInput, setChangeInput] = useState(name)
 
     const editCategoryLocal = () => {
-        editCategory(id, {id: id, categoryName: name})
+        setEdit(prevState => !prevState)
+    }
+
+    const onInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+        setChangeInput(e.currentTarget.value)
+    }
+
+    const saveChanges = () => {
+        debugger
+        editCategory(id, {id: id, categoryName: changeInput})
+        setEdit(prevState => !prevState)
+
     }
 
     return <div className={tableHeader ? styles.container_header : styles.container}>
-        <div className={styles.options}>{name}</div>
+        {edit ? <input defaultValue={name} className={styles.options} onChange={onInputChange}/> :
+            <div className={styles.options}>{name}</div>}
         <div className={styles.options}>{id}</div>
         <div style={{display: "flex", alignItems: "center"}}>
-            {tableHeader ? <div style={{width: "12rem"}}/> : <><Button buttonClass={'general'}
-                                                                       onClick={editCategoryLocal}
-                                                                       style={{marginRight: "2%"}}>
-                Edit
-            </Button>
-                <Button buttonClass={'red'} onClick={() => {
-                    console.log(`delete: ${id}`)
-                    dispatch(deleteCategory(id))
-                }}>
-                    Delete
-                </Button></>}
+            {tableHeader ? <div style={{width: "12rem"}}/> :
+                edit ?
+                    <>
+                        <Button buttonClass={'general'}
+                                onClick={saveChanges}
+                                style={{marginRight: "2%"}}>
+                            Save
+                        </Button>
+                        <Button buttonClass={'red'} onClick={editCategoryLocal}>
+                            Cancel
+                        </Button></>
+                    :
+                    <>
+                        <Button buttonClass={'general'}
+                                onClick={editCategoryLocal}
+                                style={{marginRight: "2%"}}>
+                            Edit
+                        </Button>
+                        <Button buttonClass={'red'} onClick={() => {
+                            console.log(`delete: ${id}`)
+                            dispatch(deleteCategory(id))
+                        }}>
+                            Delete
+                        </Button></>}
         </div>
     </div>
 }
